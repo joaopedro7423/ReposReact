8d07b3bf52b8f89c660f9203a9b4f96d8f49c8b8
Joao Pedro <802.018@alunos.unigran.br>
2020-12-15 10:20:58 -0300

    Buscando Repositorios

--- repositorios/src/pages/Main/index.js
+++ repositorios/src/pages/Main/index.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
-import { Title } from "./styles";
+import { Container, Form, SubmitButton } from "./styles";
+
+import { FaGithub, FaPlane } from "react-icons/fa";
+
+import api from "../../servicos/api";
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositorios, setRepositorios] = useState([]);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      async function submit() {
+        const response = await api.get(`repos/${newRepo}`);
+
+        const data = {
+          name: response.data.full_name,
+        };
+
+        setRepositorios([...repositorios, data]);
+        setNewRepo("");
+      }
+
+      submit();
+    },
+    [newRepo, repositorios]
+  );
+
+  function handleinputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
   return (
-    <Title>
-      Main
-    
-    </Title>
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositorios
+      </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar Repositorios"
+          value={newRepo}
+          onChange={handleinputChange}
+        />
+
+        <SubmitButton>
+          <FaPlane color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
